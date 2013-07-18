bfbeccd4f1b85d7fcdfe72cb0c43eda9b995440b
Edmund Sumbar <esumbar@gmail.com>
2013-07-18 12:30:15 -0600

    Cleanup and simplify add_two

--- add_two.erl
+++ add_two.erl
@@ -10,22 +10,18 @@
 -export([start/0, request/1, loop/0]).
 
 start() ->
-  process_flag(trap_exit, true),
-  Pid = spawn_link(add_two, loop, []),
-  register(add_two, Pid),
+  register(add_two, Pid = spawn_link(add_two, loop, [])),
   {ok, Pid}.
 
 request(Int) ->
   add_two ! {request, self(), Int},
   receive
-    {result, Result}       -> Result;
-    {'EXIT', _Pid, Reason} -> {error, Reason}
-    after 1000             -> timeout
+    {result, Result} -> Result
   end.
 
 loop() ->
   receive
     {request, Pid, Msg} ->
-       Pid ! {result, Msg + 2}
-  end,
-  loop().
+       Pid ! {result, Msg + 2},
+       loop()
+  end.
